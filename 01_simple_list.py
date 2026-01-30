9e265ad07538997614cc2835fd6bd541df7ced78
Muhammad Saad Raza <msaadraza49@gmail.com>
2026-01-31 00:50:13 +0500

    Basic List

---
+++ 01_simple_list.py
@@ -0,0 +1,19 @@
+# Practicing Lists
+
+li = [1,2,3,4,5,6,7,8,9]
+li_2 = [10,20,30,40,50,60,70,80,90]
+print(li)
+print(li[::-1])
+print(li[0:5:2])
+print(li + li_2)
+print(li * 2)
+print(len(li))
+print(li.count(2))
+print(li.index(2))
+print(li.pop())
+print(li.remove(2))
+print(li.clear())
+print(li.copy())
+print(li.reverse())
+print(li.sort())
+
